82ac738812e88f437d6766a7e0a852e80c2e3108
Jasper Chen <jdchen2017@gmail.com>
2024-08-23 23:30:15 -0500

    add unimplemented methods

--- src/Deck.java
+++ src/Deck.java
@@ -4,12 +4,14 @@ import java.awt.*;
 import java.awt.event.*;
 import static java.lang.System.*;
 public class Deck {
-    public class Card extends JComponent{
+    public class Card extends JComponent implements ActionListener, MouseListener{
         private int value;
         private int xPos,yPos,xSize,ySize;
         public Card(int val){
             super();
             value = val;
+            xPos = 0; yPos = 0;
+            xSize = 50; ySize = 100;
         }
         public int getValue(){return value;}
         public String toString(){return value+"";}
@@ -21,6 +23,39 @@ public class Deck {
             }else{
                 return 0;
             }
+        }
+        public Dimension getPreferredSize() {return new Dimension(xSize, ySize);}
+        public Dimension getMinimumSize() {return new Dimension(xSize, ySize );}
+        public Dimension getMaximumSize() {return new Dimension(xSize , ySize );}
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
+        }
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+        }
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+        }
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+        }
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
         }
     }
     private ArrayList<Card>deck = new ArrayList<>();
