09d4218427734b918022193bd7a1cabcbcc2a325
Jasper Chen <jdchen2017@gmail.com>
2024-08-23 23:08:21 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import static java.lang.System.*;
+import javax.swing.*;
+import java.io.*;
+public class App extends JFrame{
+    public static void main(String[] args) throws Exception {
+        App a = new App();
+    }
+    public App(){
+        super();
+        setSize(1600,900);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
