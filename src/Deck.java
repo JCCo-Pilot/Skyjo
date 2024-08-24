b0c78f6240c9789c9ae10331b8e8d812586e3b4e
Jasper Chen <jdchen2017@gmail.com>
2024-08-23 23:17:01 -0500

    card nested class in the deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,12 @@
 import java.util.*;
 import static java.lang.System.*;
 public class Deck {
-    
+    public class Card{
+        private int value;
+        public Card(int val){value = val;}
+        public int getValue(){return value;}
+    }
+    public Deck(){
 
+    }
 }
